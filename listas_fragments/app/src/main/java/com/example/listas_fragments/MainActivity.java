9f0dd38590ff2230c9d50bb03dbec0eedcbdfaf7
alvaredosergio <sergio.alvaredo@campusfp.es>
2021-11-26 13:14:51 +0100

    moviles

--- listas_fragments/app/src/main/java/com/example/listas_fragments/MainActivity.java
+++ listas_fragments/app/src/main/java/com/example/listas_fragments/MainActivity.java
@@ -23,6 +23,6 @@ public class MainActivity extends AppCompatActivity {
                         .addToBackStack(null)
                         .commit();
             }
-        }
+        });
     }
 }
